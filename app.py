1a6fe1082d214da3b152af80eef8db4a552934af
MOLLISAIVAMSI <126144005+MOLLISAIVAMSI@users.noreply.github.com>
2024-08-16 12:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     response_text = "Hello, World!"
     return jsonify({'response': response_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
